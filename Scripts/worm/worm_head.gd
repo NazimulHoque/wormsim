3ef4dd2ea41922be8b3a72e58119153744cbe8c4
ilzmatic <116411699+ilzmatic@users.noreply.github.com>
2024-07-21 11:54:09 -0400

    added joystick and changed some project settings

--- Scripts/worm/worm_head.gd
+++ Scripts/worm/worm_head.gd
@@ -1,26 +1,17 @@
 extends CharacterBody2D
-
+@onready var joy_stick = get_tree().get_first_node_in_group("Joystick")
 @export var speed: float = 500
 @export var rotation_speed: float = 7.0
 var direction: Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		input_vector.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		input_vector.y += 1
-	if Input.is_action_pressed("ui_left"):
-		input_vector.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		input_vector.x += 1
-	
-	if input_vector.length() > 0:
-		direction = input_vector.normalized()
-		var target_rotation = direction.angle() + PI / 2
-		rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
-		velocity = direction * speed
-	else:
-		velocity = Vector2.ZERO
-	move_and_slide()
+	if joy_stick:
+		direction = joy_stick.posVector
+		if direction:
+			var target_rotation = direction.angle() + PI / 2
+			rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
+			velocity = direction * speed
+		else:
+			velocity = Vector2.ZERO
+		move_and_slide()
